7c7094fb548477c0ec4ed4b22ab0ad84f832fc04
James Hurford <terrasea@gmail.com>
2011-04-06 11:03:18 +1200

    written ocaml code to convert array to list then print it to screen
    Used convert array to list by flattening the array out and making it a
    single dimension list.
    
    The list is traversed through by another function to convert the integers
    to string and appends them to a list.  This is then con-catted
    together separated by spaces into a single string.
    
    The single string that is produced is the printed to the screen.

--- learn/to_list.ml
+++ learn/to_list.ml
@@ -1,6 +1,10 @@
+open Format;;
+open String;;
+
 let table = [|[|1;2;3|];[|4;5;6|]|];;
 
-let rec to_list tablex n = if n = 0 then [] else List.append (to_list tablex (n-1)) (Array.to_list (Array.get tablex n));;
-let n = Array.length table;;
-let m = n - 1;;
-to_list table m;;
+let rec to_list tablex n = if n = 0 then [] else (to_list tablex (n-1)) @ (Array.to_list (Array.get tablex (n-1)));;
+let rec int_list_to_string_list = function
+  | [] -> []
+  | l -> [(string_of_int (List.hd l))] @ int_list_to_string_list (List.tl l);;
+print_endline (String.concat " " (int_list_to_string_list (to_list table (Array.length table))));;
