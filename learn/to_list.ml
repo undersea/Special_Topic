80f215058644f7856e6b4921f38079735617d137
James Hurford <terrasea@gmail.com>
2011-04-06 15:30:09 +1200

    getting rid of List.hd and List.tl and added a few urls of resources

--- learn/to_list.ml
+++ learn/to_list.ml
@@ -6,5 +6,5 @@ let table = [|[|1;2;3|];[|4;5;6|]|];;
 let rec to_list tablex n = if n = 0 then [] else (to_list tablex (n-1)) @ (Array.to_list (Array.get tablex (n-1)));;
 let rec int_list_to_string_list = function
   | [] -> []
-  | l -> [(string_of_int (List.hd l))] @ int_list_to_string_list (List.tl l);;
+  | l -> (string_of_int (match l with | h::t -> h | [] -> 0))::int_list_to_string_list (List.tl l);;
 print_endline (String.concat " " (int_list_to_string_list (to_list table (Array.length table))));;
