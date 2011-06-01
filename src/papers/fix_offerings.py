295b7bb2b0c3019c4098e8651fcc0eea10cd09b3
James Hurford <terrasea@gmail.com>
2011-06-02 07:58:39 +1200

    offerings are now added to each paper

--- src/papers/fix_offerings.py
+++ src/papers/fix_offerings.py
@@ -5,17 +5,27 @@ from lxml import html
 
 URL = 'http://www.massey.ac.nz/massey/learning/programme-course-paper/paper.cfm?paper_code=%s'
 
-if __name__ == '__main__' and len(sys.argv) > 0:
-    papers = etree.parse(sys.argv[0])
+if __name__ == '__main__' and len(sys.argv) > 1:
+    papers = etree.parse(sys.argv[1])
     for paper in papers.findall('./paper'):
         paper.remove(paper.find('./campus'))
         paper.remove(paper.find('./semester'))
         root = html.parse(URL % (paper.find('./code').text))
         table = root.find('.//table[@class="tbloffering"]')
-        for tr in table.findall('./tr')[1:]:
-            offering = etree.Element('offering')
-            tds = tr.findall('./td')
-            mode = etree.Element('mode')
-            mode.text = tds[1].text
-            offering.append(mode)
-            
+        try:
+            for tr in table.findall('./tr')[1:]:
+                offering = etree.Element('offering')
+                tds = tr.findall('./td')
+                mode = etree.Element('mode')
+                mode.text = str(tds[1].text != None).replace('False', '') and tds[1].text.replace('\n', '').strip()
+                offering.append(mode)
+                campus = etree.Element('campus')
+                campus.text = str(tds[3].text != None).replace('False', '') and tds[3].text.replace('\r', '').strip()
+                offering.append(campus)
+                semester = etree.Element('semester')
+                semester.text = str(tds[2].text != None).replace('False', '') and tds[2].text.replace('\r', '').strip()
+                offering.append(semester)
+                paper.find('./points').addnext(offering)
+        except:
+            pass
+    print etree.tostring(papers)
